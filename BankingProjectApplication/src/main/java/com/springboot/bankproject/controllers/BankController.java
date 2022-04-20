fc9efeed2ce78f34afe838d31f2e386975c628e5
amruth2211 <chowdaryamu99@gmail.com>
2022-04-20 12:23:27 +0530

    Bank Project

--- BankingProjectApplication/src/main/java/com/springboot/bankproject/controllers/BankController.java
+++ BankingProjectApplication/src/main/java/com/springboot/bankproject/controllers/BankController.java
@@ -1,6 +1,5 @@
 package com.springboot.bankproject.controllers;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.bankproject.model.Address;
+import com.springboot.bankproject.model.Bank;
 import com.springboot.bankproject.model.BankAccount;
-import com.springboot.bankproject.model.Branch;
+
 import com.springboot.bankproject.services.BankService;
 
 @RestController
@@ -23,25 +24,32 @@ public class BankController {
 	@Autowired
 	BankService bankService;
 	
-
-	@GetMapping("/{ifscCode}")
-	public Branch showBranchByIFSC(@PathVariable String ifscCode) {
-		return branchService.viewDetailsByIFSC(ifscCode);
-	}
-
-	@PostMapping("/create/{ifscCode}")
-	public Boolean createBranch(@RequestBody Branch branch) throws SQLException {
-			return branchService.createBranch(branch);
-	}
-
-	@GetMapping("/allAccounts")
-	public List<BankAccount> showBankAccounts() throws SQLException {
-			return branchService.showBankAccounts();
+	public List<String> getBankNames() {
+			return bankService.getBankNames();
 
 	}
 
-	@DeleteMapping("/{ifscCode}")
-	public boolean deleteBranchUsingIFSC(@PathVariable String ifscCode) {
-		return branchService.deleteByIFSC(ifscCode);
+    public Integer create(Bank bank)  {
+
+			return bankService.create(bank);
+
+    }
+    
+
+    public boolean delete(int bankCode) {
+		return bankService.delete(bankCode);
+
+    }
+
+
+    public Bank update(int bankCode,Address mainOfficAddress) {
+
+		return bankService.update(bankCode,mainOfficAddress);
+
+    }
+
+    public Bank viewBankDetails(int bankCode) {
+ 		return bankService.viewBankDetails(bankCode);
+
 	}
 }
