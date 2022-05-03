0fa5711a441df81997f97ee1d04739b9e88ccec4
amruth2211 <chowdaryamu99@gmail.com>
2022-05-03 22:19:10 +0530

    SpringBoot Complete Crud API

--- spring-boot-org-crud-api/src/main/java/com/springboot/my/org/crudapi/dao/BonusDAO.java
+++ spring-boot-org-crud-api/src/main/java/com/springboot/my/org/crudapi/dao/BonusDAO.java
@@ -6,13 +6,13 @@ import java.util.List;
 import com.springboot.my.org.crudapi.models.Bonus;
 
 public interface BonusDAO {
-	
+	//
 	public List<Bonus> getWorkerWithBonus(int id) throws SQLException;
-	
+
 	public List<Bonus> getWorkerBonusByDept(String department) throws SQLException;
-	
-	public int createBonus(int id,Bonus bonus) throws SQLException;
-	
+
+	public int createBonus(int id, Bonus bonus) throws SQLException;
+
 	public boolean deleteBonus(int id) throws SQLException;
-	
+
 }
