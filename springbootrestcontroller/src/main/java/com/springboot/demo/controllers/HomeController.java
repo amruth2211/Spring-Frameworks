fcd8aaba190bd3da38f32a57c3e9ebe04574e83d
amruth2211 <chowdaryamu99@gmail.com>
2022-05-03 22:16:38 +0530

    SpringBoot RestController

--- springbootrestcontroller/src/main/java/com/springboot/demo/controllers/HomeController.java
+++ springbootrestcontroller/src/main/java/com/springboot/demo/controllers/HomeController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HomeController {
+	//
 	@GetMapping("/")
 	public String home() {
-		return  "Hello visitor!";
+		return "Hello visitor!";
 	}
-} 
+}
